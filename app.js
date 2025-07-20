280affbff7710f4ec9127dbfa93f64898859fc56
srivastavHimanshu4503 <codeforinterviewwithhimanshu@gmail.com>
2025-07-21 01:53:34 +0530

    Remove the root directory

--- app.js
+++ app.js
@@ -82,9 +82,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/", (req, res) => {
-  res.send("Hi, I am root!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root!");
+// });
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
